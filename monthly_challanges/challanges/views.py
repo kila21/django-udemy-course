f09b4c76c2ee6bd08fd9f9d75493bf5097ef0c79
kila <luka.kilasonia.1@btu.edu.ge>
2024-08-06 01:52:35 +0400

    [challanges] update view for all month

--- monthly_challanges/challanges/views.py
+++ monthly_challanges/challanges/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('This Works!')
+def monthly_challange(request, month):
+    if month == 'january':
+        challange_text = 'january challange'
+    elif month == 'february':
+        challange_text = 'february challange'
+    elif month == 'march':
+        challange_text = 'march challange'
+    else:
+        return HttpResponseNotFound('Month is not supported')
+    return HttpResponse(challange_text)

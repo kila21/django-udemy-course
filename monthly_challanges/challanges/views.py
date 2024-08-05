8c29bec77aa2a5908f6e9186b2cde224ccf9db8a
kila <luka.kilasonia.1@btu.edu.ge>
2024-08-06 02:10:41 +0400

    [challanges] create month dict to display data

--- monthly_challanges/challanges/views.py
+++ monthly_challanges/challanges/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+monthly_challanges = {
+    'january': 'Eat No Meat for the entire month!',
+    'february': 'Walk for at least 20 min every day!',
+    'march': 'Learn Django for at least 20 min every day!',
+    'april': 'april challange',
+    'may': 'may challange',
+    'june': 'june challange',
+    'july': 'july challange',
+    'august': 'august challange',
+    'september': 'september challange',
+    'octomber': 'octomber challange',
+    'november': 'november challange',
+    'december': 'december challange',
+}
+
 # Create your views here.
 def monthly_challange_by_number(request, month):
     return HttpResponse(month)
 
 def monthly_challange(request, month):
-    if month == 'january':
-        challange_text = 'january challange'
-    elif month == 'february':
-        challange_text = 'february challange'
-    elif month == 'march':
-        challange_text = 'march challange'
-    else:
-        return HttpResponseNotFound('Month is not supported')
+    try:
+        challange_text = monthly_challanges[month]
+    except:
+        return HttpResponseNotFound('Month isnt Supported!')
+    
     return HttpResponse(challange_text)

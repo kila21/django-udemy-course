5d6635a84630a02ba5cf5cd25ac618ae814e199d
kila <luka.kilasonia.1@btu.edu.ge>
2024-08-31 03:57:08 +0400

    use get_absolute_url for linking

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
@@ -10,6 +11,9 @@ class Book(models.Model):
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
 
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
+    
 
     def __str__(self):
         return f'{self.title} ({self.rating})'

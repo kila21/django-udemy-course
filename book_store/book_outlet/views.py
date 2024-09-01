cf4732a34ac5c62e345670b707d3edd2ab4a38f1
kila <luka.kilasonia.1@btu.edu.ge>
2024-09-01 14:41:45 +0400

    use slug field and update for links to navigate

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -9,13 +9,13 @@ def index(request):
     books = Book.objects.all()
     return render(request, 'book_outlet/index.html', {'books': books})
 
-def book_detail(request, id):
+def book_detail(request, slug):
     # try:
     #     book = Book.objects.get(pk=id)
     # except:
     #     raise Http404()
     
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, 
                   'book_outlet/book_detail.html', 
                   {'author': book.author,

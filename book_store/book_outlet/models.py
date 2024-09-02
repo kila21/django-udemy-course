c4783574c27c5d9050cbc888ee0a57d62d6317a2
kila <luka.kilasonia.1@btu.edu.ge>
2024-09-02 21:31:59 +0400

    make slug blank to True

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.text import slugify
 
 # Create your models here.
@@ -8,10 +8,10 @@ from django.utils.text import slugify
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField(validators=[MinLengthValidator(1), MaxLengthValidator(5)])
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default='', null=False, db_index=True)
+    slug = models.SlugField(default='', blank=True, null=False, db_index=True)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])

307ba466a9f1de955355e9c6a0560d938f033a01
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-01 15:23:30 +0400

    add invalid form warning

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -6,10 +6,16 @@ from django.http import HttpResponseRedirect
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
+        if entered_username == '':
+            return render(request, 'reviews/review.html', {
+            'has_error': True
+        })
         print(entered_username)
         return HttpResponseRedirect('/thank-you')
     else:
-        return render(request, 'reviews/review.html')
+        return render(request, 'reviews/review.html', {
+            'has_error': False
+        })
 
 
 def thank_you(request):

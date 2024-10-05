aabdbdac3a5f9817d7ef350ef0d7ed3ea9df106f
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-05 18:25:46 +0400

    add new form inputs

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -7,4 +7,5 @@ class ReviewFrom(forms.Form):
         'required': 'Your name must be not empty!',
         'max_length': 'Please enter a shorter name!'
     })
- 
+    review_text = forms.CharField(label='Your Feedback', max_length=200, widget=forms.Textarea)
+    rating = forms.IntegerField(label='Your Rating', min_value=1, max_value=5)

cba5d996dd7317808ea76756793dc96eb5c213a6
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-05 13:05:13 +0400

    change forms error messages

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -3,4 +3,8 @@ from django import forms
 
 
 class ReviewFrom(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label='Your Name', max_length=100, error_messages={
+        'required': 'Your name must be not empty!',
+        'max_length': 'Please enter a shorter name!'
+    })
+ 

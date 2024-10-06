73fdaf7fb28b5bc88bf5eea71e83e20a9d048b15
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-06 17:06:48 +0400

    remove review data

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -10,9 +10,6 @@ def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            review = Review(user_name=form.cleaned_data['user_name'], 
-                            review_text=form.cleaned_data['review_text'], 
-                            rating=form.cleaned_data['rating'])
             review.save()
             return HttpResponseRedirect('/thank-you')
         

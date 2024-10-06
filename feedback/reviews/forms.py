54d493b8df3e48a8e1af37741566c971d3d32e4a
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-06 16:59:30 +0400

    change forms.form to forms.Modelform

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,11 +1,18 @@
 
 from django import forms 
+from .models import Review
 
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label='Your Name', max_length=100, error_messages={
-        'required': 'Your name must be not empty!',
-        'max_length': 'Please enter a shorter name!'
-    })
-    review_text = forms.CharField(label='Your Feedback', max_length=200, widget=forms.Textarea)
-    rating = forms.IntegerField(label='Your Rating', min_value=1, max_value=5)
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label='Your Name', max_length=100, error_messages={
+#         'required': 'Your name must be not empty!',
+#         'max_length': 'Please enter a shorter name!'
+#     })
+#     review_text = forms.CharField(label='Your Feedback', max_length=200, widget=forms.Textarea)
+#     rating = forms.IntegerField(label='Your Rating', min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        # exclude = ['user_name']

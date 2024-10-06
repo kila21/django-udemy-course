30933a43c601b0e491e342f0f4125d341045d4b4
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-06 17:25:09 +0400

    update review as class bases view

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .models import Review
+from django.views import View
 
+from .models import Review
 from .forms import ReviewForm
 # Create your views here.
-
-
-def review(request):
-    if request.method == 'POST':
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
+    
+    def post(self, request):
         form = ReviewForm(request.POST)
         if form.is_valid():
-            review.save()
+            form.save()
             return HttpResponseRedirect('/thank-you')
-        
-    else:
-        form = ReviewForm()
-
-    return render(request, 'reviews/review.html', {
-        'form': form
-    })
-
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')

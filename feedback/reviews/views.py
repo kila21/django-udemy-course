be054052d4b1c01c9e4d1ca469a436592efc8c1a
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-07 17:00:47 +0400

    update main view with FromView

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -4,25 +4,29 @@ from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from .models import Review
 from .forms import ReviewForm
 # Create your views here.
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, 'reviews/review.html', {
-            'form': form
-        })
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = 'reviews/review.html'
+    success_url = '/thank-you'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
     
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/thank-you')
-        return render(request, 'reviews/review.html', {
-            'form': form
-        })
+
+    # def post(self, request):
+    #     form = ReviewForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect('/thank-you')
+    #     return render(request, 'reviews/review.html', {
+    #         'form': form
+    #     })
 
 
 class ThankYouView(TemplateView):

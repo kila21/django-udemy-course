a46e4e6d8be8f4bc9bd37aa27d05fec7fd09c2fa
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-11 17:05:20 +0400

    add root media link

--- feedback/feedback/settings.py
+++ feedback/feedback/settings.py
@@ -118,8 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+

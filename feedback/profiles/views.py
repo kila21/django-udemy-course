deaaa2d7f2554d0c7f0d454e6965b8f799495938
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-11 17:05:42 +0400

    create model to store image in database

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-
-# Create your views here.
-
 from django.shortcuts import render
 from django.views import View
 
-from .forms import ProfileForm
-
 # Create your views here.
 
+from .forms import ProfileForm
+from .models import UserProfile
 
-def store_file(file):
-    with open('temp/screenshot.jpg', 'wb+') as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+# Create your views here.
 
 class CreateProfileView(View):
     def get(self, request):
@@ -24,7 +18,9 @@ class CreateProfileView(View):
     def post(self, request):
         subbmited_form = ProfileForm(request.POST, request.FILES)
         if subbmited_form.is_valid():
-            store_file(request.FILES['image'])
+            # store_file(request.FILES['image'])
+            profile = UserProfile(image=request.FILES['user_image'])
+            profile.save()
             return HttpResponseRedirect('/profiles') 
 
         return render(request, "profiles/create_profile.html", {'form': subbmited_form})

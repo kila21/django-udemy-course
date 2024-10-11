407c16bb30c29333b066a2c68dd072b501d083ba
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-11 17:52:36 +0400

    serve uploaded file into the page

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -16,3 +17,9 @@ class CreateProfileView(CreateView):
     model = UserProfile
     fields = '__all__'
     success_url = '/profiles'
+
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = 'profiles/user_profiles.html'
+    context_object_name = 'profiles'

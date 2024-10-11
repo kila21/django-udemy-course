5b6210652ef8b832d68a1c626d88ecf7dbc53a5e
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-11 15:35:31 +0400

    custom file upload function

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -8,9 +9,15 @@ from django.views import View
 # Create your views here.
 
 
+def store_file(file):
+    with open('temp/screenshot.jpg', 'wb+') as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        store_file(request.FILES['image'])
+        return HttpResponseRedirect('/profiles')

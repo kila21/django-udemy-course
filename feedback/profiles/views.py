62a14e82b27756a7f1a2aa14339397dc589c2d41
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-11 17:29:25 +0400

    update view as CreateView from generic.edit

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
@@ -10,17 +11,8 @@ from .models import UserProfile
 
 # Create your views here.
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {'form': form})
-
-    def post(self, request):
-        subbmited_form = ProfileForm(request.POST, request.FILES)
-        if subbmited_form.is_valid():
-            # store_file(request.FILES['image'])
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect('/profiles') 
-
-        return render(request, "profiles/create_profile.html", {'form': subbmited_form})
+class CreateProfileView(CreateView):
+    template_name = 'profiles/create_profile.html'
+    model = UserProfile
+    fields = '__all__'
+    success_url = '/profiles'
